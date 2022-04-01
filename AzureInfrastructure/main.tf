04752acbb0071082f41b68ec012e824100948683
Eric Lauret <eric.lauret@live.com>
2022-04-02 00:20:46 +0200

    Create IoT hub

--- AzureInfrastructure/main.tf
+++ AzureInfrastructure/main.tf
@@ -8,3 +8,19 @@ resource "azurerm_resource_group" "solarpanel_rg" {
 }
 
 # Create IoT hub
+resource "azurerm_iothub" "solarpanel_iot" {
+  name = "SOLAR-PANEL-${upper(var.env)}-001-IOT"
+  resource_group_name = azurerm_resource_group.solarpanel_rg.name
+  location = azurerm_resource_group.solarpanel_rg.location
+  sku {
+    name = "F1"
+    capacity = "1"
+  }
+  tags = {
+      Environment = upper(var.env)
+  }
+
+  depends_on = [
+    azurerm_resource_group.solarpanel_rg
+  ]
+}
